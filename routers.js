e71f07245df34a4fef14281b08628e6050b96b71
sw08 <lswja6866@naver.com>
2023-02-25 21:11:34 +0900

    [FIX] Restruct routers

--- routers.js
+++ routers.js
@@ -8,50 +8,61 @@ class Routers {
         this.connection.query('CREATE TABLE IF NOT EXISTS personalbest(guid CHAR(17), laptime INT, model CHAR(15), track CHAR(30))');
         this.connection.query('CREATE TABLE IF NOT EXISTS trackbest(guid CHAR(17), laptime INT, model CHAR(15), track CHAR(30))');
         this.connection.query('CREATE TABLE IF NOT EXISTS username(guid CHAR(17), username CHAR(30))');
-        this.routers = []
-    }
-    add_router(type, route, router) {
-        this.routers.push({route: route, router: router, type: type});
+        this.routers = [
+            {
+                route: '/personalbest',
+                router: (req, res) => {
+                    this.connection.query(`SELECT laptime FROM personalbest WHERE guid=${req.query.guid} AND model='${req.query.model}' AND track='${req.query.track}'`, (error, results) => {
+                        return {laptime: results.length > 0 ? results[0].laptime : 0};
+                    });
+                }
+            },
+            {
+                route: '/trackbest',
+                router: (req, res) => {
+                    this.connection.query(`SELECT * FROM trackbest WHERE model='${req.query.model}' AND track='${req.query.track}'`, (error, results) => {
+                        this.connection.query(`SELECT username FROM username WHERE guid='${req.query.guid}`, (error, results2) => {
+                            return results.length > 0 ? {laptime: results[0].laptime, username: results2[0].username, guid: results[0].guid} : undefined;
+                        })
+                    });
+                }
+            },
+            {
+                route: '/personalbest',
+                router: (req, res) => {
+                    this.connection.query(`SELECT * FROM personalbest WHERE track=${req.query.track} AND model=${req.query.model} AND guid=${req.query.guid}`, (error, results) => {
+                        if (results.length > 0) this.connection.query(`UPDATE personalbest SET guid=${req.query.guid} AND laptime=${req.body.laptime} WHERE track='${this.track}' AND model='${req.query.model}'`, (error, results) => {});
+                        else this.connection.query(`INSERT INTO personalbest (guid, laptime, track, model) VALUES(${req.query.guid}, ${req.body.laptime}, '${this.track}', '${req.query.model}')`, (error, results) => {});
+                    });
+                },
+                type: 'post'
+            },
+            {
+                route: '/trackbest',
+                router: (req, res) => {
+                    this.connection.query(`SELECT * FROM trackbest WHERE track=${req.query.track} AND model=${req.query.model}`, (error, results) => {
+                        if (results.length > 0) this.connection.query(`UPDATE personalbest SET guid=${req.body.guid} AND laptime=${req.body.laptime} WHERE track='${this.track}' AND model='${req.query.model}'`, (error, results) => {});
+                        else this.connection.query(`INSERT INTO trackbest (guid, laptime, track, model) VALUES(${req.body.guid}, ${req.body.laptime}, '${this.track}', '${req.query.model}')`, (error, results) => {});
+                    });
+                },
+                type: 'post'
+            },
+            {
+                route: '/username',
+                router: (req, res) => {
+                    this.connection.query(`SELECT username FROM username WHERE guid=${req.query.guid}`, (err, results) => {
+                        if (results.length > 0) {
+                            if (results[0].username !== req.body.username) this.connection.query(`UPDATE username SET username=? WHERE guid=${req.query.guid}`, [req.body.username], (error, results) => {});
+                        } else {
+                            this.connection.query(`INSERT INTO username (username, guid) VALUES(?, ${req.query.guid})`, [req.body.username], (error, results) => {});
+                        }
+                    });
+                },
+                type: 'post'
+            }
+        ]
     }
 }
 
-const router = new Routers();
 
-router.add_router('get', '/personalbest', (req, res) => {
-    this.connection.query(`SELECT laptime FROM personalbest WHERE guid=${req.query.guid} AND model='${req.query.model}' AND track='${req.query.track}'`, (error, results) => {
-        return {laptime: results.length > 0 ? results[0].laptime : 0};
-    });
-});
-router.add_router('get', '/trackbest', (req, res) => {
-    this.connection.query(`SELECT * FROM trackbest WHERE model='${req.query.model}' AND track='${req.query.track}'`, (error, results) => {
-        this.connection.query(`SELECT username FROM username WHERE guid='${req.query.guid}`, (error, results2) => {
-            return results.length > 0 ? {laptime: results[0].laptime, username: results2[0].username, guid: results[0].guid} : undefined;
-        })
-    });
-});
-
-router.add_router('post', '/personalbest', (req, res) => {
-    this.connection.query(`SELECT * FROM personalbest WHERE track=${req.query.track} AND model=${req.query.model} AND guid=${req.query.guid}`, (error, results) => {
-        if (results.length > 0) this.connection.query(`UPDATE personalbest SET guid=${req.query.guid} AND laptime=${req.body.laptime} WHERE track='${this.track}' AND model='${req.query.model}'`, (error, results) => {});
-        else this.connection.query(`INSERT INTO personalbest (guid, laptime, track, model) VALUES(${req.query.guid}, ${req.body.laptime}, '${this.track}', '${req.query.model}')`, (error, results) => {});
-    });
-});
-router.add_router('post', '/trackbest', (req, res) => {
-    this.connection.query(`SELECT * FROM trackbest WHERE track=${req.query.track} AND model=${req.query.model}`, (error, results) => {
-        if (results.length > 0) this.connection.query(`UPDATE personalbest SET guid=${req.body.guid} AND laptime=${req.body.laptime} WHERE track='${this.track}' AND model='${req.query.model}'`, (error, results) => {});
-        else this.connection.query(`INSERT INTO trackbest (guid, laptime, track, model) VALUES(${req.body.guid}, ${req.body.laptime}, '${this.track}', '${req.query.model}')`, (error, results) => {});
-    });
-});
-
-router.add_router('post', '/username', (req, res) => {
-    this.connection.query(`SELECT username FROM username WHERE guid=${req.query.guid}`, (err, results) => {
-        if (results.length > 0) {
-            if (results[0].username !== req.body.username) this.connection.query(`UPDATE username SET username=? WHERE guid=${req.query.guid}`, [req.body.username], (error, results) => {});
-        } else {
-            this.connection.query(`INSERT INTO username (username, guid) VALUES(?, ${req.query.guid})`, [req.body.username], (error, results) => {});
-        }
-    });
-})
-
-
-module.exports = router;
+module.exports = new Routers();
