fa273a25720ef1e0004a1b5f23195053b17160d9
sw08 <lswja6866@naver.com>
2023-02-26 11:49:06 +0900

    [FIX] Edit expression

--- routers.js
+++ routers.js
@@ -13,7 +13,7 @@ class Routers {
                 route: '/personalbest',
                 router: (req, res) => {
                     this.connection.query(`SELECT laptime FROM personalbest WHERE guid=${req.query.guid} AND model='${req.query.model}' AND track='${req.query.track}'`, (error, results) => {
-                        res.json({laptime: results.length > 0 ? results[0].laptime : 0});
+                        res.json({laptime: results !== undefined || results.length > 0 ? results[0].laptime : 0});
                     });
                 }
             },
@@ -22,7 +22,7 @@ class Routers {
                 router: (req, res) => {
                     this.connection.query(`SELECT * FROM trackbest WHERE model='${req.query.model}' AND track='${req.query.track}'`, (error, results) => {
                         this.connection.query(`SELECT username FROM username WHERE guid='${req.query.guid}`, (error, results2) => {
-                            res.json(results.length > 0 ? {laptime: results[0].laptime, username: results2[0].username, guid: results[0].guid} : undefined);
+                            res.json(results !== undefined || results.length > 0 ? {laptime: results[0].laptime, username: results2[0].username, guid: results[0].guid} : undefined);
                         })
                     });
                 }
@@ -31,7 +31,7 @@ class Routers {
                 route: '/personalbest',
                 router: (req, res) => {
                     this.connection.query(`SELECT * FROM personalbest WHERE track=${req.query.track} AND model=${req.query.model} AND guid=${req.query.guid}`, (error, results) => {
-                        if (results.length > 0) this.connection.query(`UPDATE personalbest SET guid=${req.query.guid} AND laptime=${req.body.laptime} WHERE track='${this.track}' AND model='${req.query.model}'`, (error, results) => {});
+                        if (results !== undefined || results.length > 0) this.connection.query(`UPDATE personalbest SET guid=${req.query.guid} AND laptime=${req.body.laptime} WHERE track='${this.track}' AND model='${req.query.model}'`, (error, results) => {});
                         else this.connection.query(`INSERT INTO personalbest (guid, laptime, track, model) VALUES(${req.query.guid}, ${req.body.laptime}, '${this.track}', '${req.query.model}')`, (error, results) => {});
                     });
                 },
@@ -41,7 +41,7 @@ class Routers {
                 route: '/trackbest',
                 router: (req, res) => {
                     this.connection.query(`SELECT * FROM trackbest WHERE track=${req.query.track} AND model=${req.query.model}`, (error, results) => {
-                        if (results.length > 0) this.connection.query(`UPDATE personalbest SET guid=${req.body.guid} AND laptime=${req.body.laptime} WHERE track='${this.track}' AND model='${req.query.model}'`, (error, results) => {});
+                        if (results !== undefined || results.length > 0) this.connection.query(`UPDATE personalbest SET guid=${req.body.guid} AND laptime=${req.body.laptime} WHERE track='${this.track}' AND model='${req.query.model}'`, (error, results) => {});
                         else this.connection.query(`INSERT INTO trackbest (guid, laptime, track, model) VALUES(${req.body.guid}, ${req.body.laptime}, '${this.track}', '${req.query.model}')`, (error, results) => {});
                     });
                 },
@@ -51,7 +51,7 @@ class Routers {
                 route: '/username',
                 router: (req, res) => {
                     this.connection.query(`SELECT username FROM username WHERE guid=${req.query.guid}`, (err, results) => {
-                        if (results.length > 0) {
+                        if (results !== undefined || results.length > 0) {
                             if (results[0].username !== req.body.username) this.connection.query(`UPDATE username SET username=? WHERE guid=${req.query.guid}`, [req.body.username], (error, results) => {});
                         } else {
                             this.connection.query(`INSERT INTO username (username, guid) VALUES(?, ${req.query.guid})`, [req.body.username], (error, results) => {});
